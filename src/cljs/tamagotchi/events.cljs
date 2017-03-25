4c27b497466bd76b435a1f9d601eb63811065d89
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-25 23:42:20 +0100

    add timed :tic event and handler

--- src/cljs/tamagotchi/events.cljs
+++ src/cljs/tamagotchi/events.cljs
@@ -1,8 +1,19 @@
 (ns tamagotchi.events
-    (:require [re-frame.core :as re-frame]
-              [tamagotchi.db :as db]))
+  (:require [re-frame.core :as re-frame]
+            [tamagotchi.db :as db]
+            [tamagotchi.logic :as logic]))
 
 (re-frame/reg-event-db
  :initialize-db
  (fn  [_ _]
    db/default-db))
+
+(re-frame/reg-event-db
+  :tic
+  (fn  [db _]
+    (logic/tic db)))
+
+(defn dispatch-timer-event []
+  (re-frame/dispatch [:tic]))
+
+(defonce do-timer (js/setInterval dispatch-timer-event 1000))
