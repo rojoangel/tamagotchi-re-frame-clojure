5d0edb732989b80543afe1962eb315e48e6b3387
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-25 23:02:33 +0100

    add hungriness view and sub

--- src/cljs/tamagotchi/subs.cljs
+++ src/cljs/tamagotchi/subs.cljs
@@ -6,3 +6,8 @@
  :name
  (fn [db]
    (:name db)))
+
+(re-frame/reg-sub
+  :hungriness
+  (fn [db]
+    (:hungriness db)))
--- src/cljs/tamagotchi/views.cljs
+++ src/cljs/tamagotchi/views.cljs
@@ -1,7 +1,11 @@
 (ns tamagotchi.views
-    (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [:name])]
+  (let [name (re-frame/subscribe [:name])
+        hungriness (re-frame/subscribe [:hungriness])]
     (fn []
-      [:div "name: " @name])))
+      [:div
+       [:div "name: " @name]
+       [:div "hungriness: " @hungriness]
+       ])))
